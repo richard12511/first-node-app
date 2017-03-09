ad3c5140c91d85f80c441443f9b6dfaa1e851f5a
richard12511 <richardnew12511@gmail.com>
2017-03-09 16:05:30 -0600

    adds http server

--- app/index.js
+++ app/index.js
@@ -1,4 +1,17 @@
-const calc = require('./calc')
-const numbersToAdd = [3,4,10,2]
-const result = calc.sum(numbersToAdd)
-console.log(`The result is: ${result}`)
+const http = require('http');
+const port = 3000
+
+const requestHandler = (request, response) => {
+  console.log(request.url);
+  response.end('Hello Node.js Server!')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if(err){
+    return console.log('something bad happened', err);
+  }
+
+  console.log(`server listening on ${port}`);
+})
